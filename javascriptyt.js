69d228bfa08d6152726cba50f0000e559fab2822
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-10-01 12:55:16 -0300

    api

---
+++ javascriptyt.js
@@ -0,0 +1,18 @@
+let key="AIzaSyAiKq69zlkGWrwQyWr46SVefw42K8kq-zU";
+let idCanal="UCekiUWTObDKZnaeS-reYQbg";
+let resPorPagina=5;
+let url="https://www.googleapis.com/youtube/v3/search?key=" + key + "&channelId=" + idCanal + "&part=snippet,id&order=date&maxResults=" + resPorPagina;
+let xmlhttp=new XMLHttpRequest();
+xmlhttp.open('GET',url,true);
+xmlhttp.send();
+xmlhttp.onreadystatechange=function(){
+    if(this.readyState==4 && this.status==200){
+        let data=JSON.parse(this.responseText);
+        console.log(data);
+        for(var i=0;i<data.items.length;i++){
+            console.log(data.items[i].snippet.title);
+        }
+    }
+}//https://www.youtube.com/watch?v=qbsNAuyo2pM
+// pagina de visualización del proyecto con json
+//https://www.googleapis.com/youtube/v3/search?key=AIzaSyAiKq69zlkGWrwQyWr46SVefw42K8kq-zU&channelId=UCekiUWTObDKZnaeS-reYQbg&part=snippet,id&order=date&maxResults=5
